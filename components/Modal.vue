c822040fbb457261bddb2033fedae3b6316c59e7
Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>
2025-06-01 01:04:31 +0200

    feat: add simple Modal component
    Modal component has basic props to see if it's visible
    
    Signed-off-by: Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>

---
+++ components/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="modalValue" class="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+    <div class="relative bg-white rounded-lg shadow-lg w-96">
+      <button class="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
+        X
+      </button>
+      <div class="p-6">
+        <h2 class="text-2xl font-bold mb-4">
+          This is Modal
+        </h2>
+        <p class="text-gray-700">
+          Paragraph text in the modal
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  modalValue: {
+    type: Boolean,
+    required: true
+  }
+})
+
+</script>
+
+<style></style>
