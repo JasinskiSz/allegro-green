a98fafae4bffe56ac85a943baa9f8803d88ed764
Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>
2025-06-02 00:25:38 +0200

    feat: implement close Modal functionality
    Signed-off-by: Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-if="modalValue" class="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+  <div v-if="visible" class="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
     <div class="relative bg-white rounded-lg shadow-lg w-96">
-      <button class="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
+      <button @click="emitClose" class="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
         X
       </button>
       <div class="p-6">
@@ -18,11 +18,17 @@
 
 <script lang="ts" setup>
 const props = defineProps({
-  modalValue: {
+  visible: {
     type: Boolean,
     required: true
   }
-})
+});
+
+const emit = defineEmits(['update:visible']);
+
+const emitClose = () => {
+  emit('update:visible', false);
+};
 
 </script>
 
