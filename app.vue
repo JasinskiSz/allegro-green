8c40763c9a7d14692fdf3a29c2042c411b215af5
Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>
2025-06-02 00:25:47 +0200

    feat: create open modal button
    Signed-off-by: Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <button @click="openModal" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      open modal
+    </button>
+    <Modal v-model:visible="isModalOpen" />
   </div>
 </template>
+<script setup lang="ts">
+const isModalOpen = ref(false);
+
+const openModal = () => {
+  isModalOpen.value = true;
+}
+
+</script>
